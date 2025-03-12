00bc8334bc8d92dae44d60d2e132f9d1d00b5b1b
Luke Nguyen <luke.nguyen990@gmail.com>
2025-03-13 06:18:07 +0700

    config: add swagger and env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config as dotenvConfig } from 'dotenv';
+import { AppModule } from './module';
+
+dotenvConfig({ path: '.env' });
+bootstrap();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pepperstone Take Home Test API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app
+    .listen(process.env.SERVER_PORT, process.env.SERVER_HOST)
+    .then(() => {});
 }
-bootstrap();
